49a2b16929e42903775df51285d30ce4bb869613
n20186005-hash <n20186005@gmail.com>
2026-07-29 20:17:48 +0800

    更新

---
+++ src/data/site.ts
@@ -0,0 +1,17 @@
+export const site = {
+  name: '石門大草坪',
+  domain: 'ShimenGreatLawn.com',
+  url: 'https://shimengreatlawn.com',
+  title: '石門大草坪｜停車、交通、流蘇花季與親子野餐指南',
+  description:
+    '桃園龍潭石門大草坪完整行前指南，整理交通、停車、廁所、野餐、放風箏、寵物、流蘇花季與十一份周邊行程。',
+  locale: 'zh_TW',
+  lang: 'zh-Hant-TW',
+  gaId: 'G-HXM22WWPKP',
+  address: '桃園市龍潭區佳安里佳安路2號周邊',
+  latitude: 24.83509,
+  longitude: 121.24165,
+  updated: '2026-07-29',
+  mapUrl:
+    'https://www.google.com/maps/dir/?api=1&destination=24.83509%2C121.24165&travelmode=driving',
+} as const;
